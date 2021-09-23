b3d0ea2dfa18e589c7df10b5e5d0e61d269f0a9b
dhanyar017 <dhanyaracharya017@gmail.com>
2021-09-23 14:22:39 -0400

    writes increment program

---
+++ IncrementOperationDemo.java
@@ -0,0 +1,17 @@
+class IncrementOperationDemo
+{
+public static void main(String[] args)
+{
+System.out.println("Pre-Increment Operation");
+int a=1;
+int b=++a;
+System.out.println("a:"+a);
+System.out.println("b:"+b);
+
+System.out.println("Post-Increment Operation");
+int i=100;
+int j=i++;
+System.out.println("i:"+i);
+System.out.println("j:"+j);
+}
+}
