03215f975be60b6e6243c91838a6df56b8ee61d3
dhanyar017 <dhanyaracharya017@gmail.com>
2021-09-23 14:13:33 -0400

    writes arithmetic program

---
+++ ArithmeticOperationsDemo.java
@@ -0,0 +1,25 @@
+class ArithmeticOperationsDemo
+{
+public static void main(String[] args)
+{
+int a=10;
+int b=20;
+int addResult=a+b;
+System.out.println("addResult : "+addResult);
+
+int x=100;
+int y=50;
+int subResult=x-y;
+System.out.println("subResult : "+subResult);
+
+int p=5;
+int q=10;
+int mulResult=p*q;
+System.out.println("mulResult : "+mulResult);
+
+int m=9;
+int n=2;
+int modResult=m%n;
+System.out.println("modResult : "+modResult);
+}
+}
