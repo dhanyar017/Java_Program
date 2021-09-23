8fb4cdda70471cc38620101421282118aff41ad5
dhanyar017 <dhanyaracharya017@gmail.com>
2021-09-23 14:15:38 -0400

    writes arithmetic program

---
+++ ArithmeticOperation.java
@@ -0,0 +1,29 @@
+class ArithmeticOperation
+{
+public static void main(String[] args)
+{
+System.out.println(".....Addition.......");
+int a=56;
+int b=67;
+int add=a+b;
+System.out.println("Addition result : "+add);
+
+System.out.println("......Subtraction.......");
+int c=80;
+int d=20;
+int sub=c-d;
+System.out.println("Subtraction result : "+sub);
+
+System.out.println("..........Multiplication.....");
+int m=20;
+int n=10;
+int mul=m*n;
+System.out.println("Multiplication result : "+mul);
+
+System.out.println("..........Modulus.....");
+int x=27;
+int y=3;
+int mod=x%y;
+System.out.println("Modulus result : "+mod);
+}
+}
