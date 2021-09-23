1428cac28c003c525128a5e65d81995e66557c86
dhanyar017 <dhanyaracharya017@gmail.com>
2021-09-23 14:19:07 -0400

    writes decrement program

---
+++ DecrementOperationDemo.java
@@ -0,0 +1,15 @@
+class DecrementOperationDemo
+{
+public static void main(String[] args)
+{
+int m=10;
+int n=--m;
+System.out.println("m:"+m);
+System.out.println("n:"+n);
+
+int i=50;
+int j=i--;
+System.out.println("i:"+i);
+System.out.println("j:"+j);
+}
+}
