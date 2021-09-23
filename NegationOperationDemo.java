323897d75f9514cffee871aca4cdd2b8b230d918
dhanyar017 <dhanyaracharya017@gmail.com>
2021-09-23 14:27:30 -0400

    writes negation program

---
+++ NegationOperationDemo.java
@@ -0,0 +1,10 @@
+class NegationOperationDemo
+{
+public static void main(String[] args)
+{
+boolean b=true;
+boolean result=!b;
+System.out.println("b:"+b);
+System.out.println("result :"+result);
+}
+}
