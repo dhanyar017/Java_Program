89f5ad4fa5c4113e6c1dbeb4fdbc3213092f7ec8
dhanyar017 <dhanyaracharya017@gmail.com>
2021-09-25 08:28:28 -0400

    writes relational operator program

---
+++ RelationalOperatorDemo.java
@@ -0,0 +1,29 @@
+class RelationalOperatorDemo
+{
+public static void main(String[] args)
+{
+int i=10;
+int j=20;
+int k=20;
+boolean result1 = i<j;
+System.out.println(result1);
+boolean result2 = j<i;
+System.out.println(result2);
+boolean result3 = i>j;
+System.out.println(result3);
+boolean result4 = j>i;
+System.out.println(result4);
+boolean result5 = i<=k;
+System.out.println(result5);
+boolean result6 = i>=k;
+System.out.println(result6);
+boolean result7 = i==k;
+System.out.println(result7);
+boolean result8 = i!=k;
+System.out.println(result8);
+boolean result9 = j!=k;
+System.out.println(result9);
+boolean result10 = j==k;
+System.out.println(result10);
+}
+}
