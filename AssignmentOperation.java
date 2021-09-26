641d44c4eaa5579532660919b78654bea2aef09d
dhanyar017 <dhanyaracharya017@gmail.com>
2021-09-26 08:16:45 -0400

    writes assignment operator program

---
+++ AssignmentOperation.java
@@ -0,0 +1,31 @@
+class AssignmentOperation
+{
+public static void main(String[] args)
+{
+int a=10;
+int b=20;
+System.out.println("....ADDITION......");
+a+= b;
+System.out.println("a : "+a);
+a+= 5;
+System.out.println("a : "+a);
+
+System.out.println("......SUBTRACTION........");
+a-= b;
+System.out.println("a : "+a);
+a-= 8;
+System.out.println("a : "+a);
+
+System.out.println("........MULTIPLICATION......");
+a*= b;
+System.out.println("a : "+a);
+a*= 3;
+System.out.println("a : "+a);
+
+System.out.println(".....DIVISION......");
+a/= b;
+System.out.println("a : "+a);
+a/= 2;
+System.out.println("a : "+a);
+}
+}
