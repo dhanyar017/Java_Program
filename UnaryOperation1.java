0541e917f21f9441bcf11ccb02c0d7fa2d35978f
dhanyar017 <dhanyaracharya017@gmail.com>
2021-09-26 08:39:57 -0400

    writes unary operator program

---
+++ UnaryOperation1.java
@@ -0,0 +1,16 @@
+class UnaryOperation1
+{
+public static void main(String[] args)
+{
+int x=10;
+System.out.println(".......USING --.......");
+System.out.println(x--);
+System.out.println(--x);
+System.out.println(x-- + x--);
+System.out.println(--x + --x);
+System.out.println(--x + x--);
+System.out.println(x-- + --x);
+}
+}
+
+
