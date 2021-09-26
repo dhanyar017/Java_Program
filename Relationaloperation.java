156346a5441cb588f1291d63123703de6f6c7696
dhanyar017 <dhanyaracharya017@gmail.com>
2021-09-26 08:35:59 -0400

    writes relational operator program

---
+++ Relationaloperation.java
@@ -0,0 +1,22 @@
+class Relationaloperation
+{
+public static void main(String[] args)
+{
+int i=50;
+int j=30;
+System.out.println(i<j);
+System.out.println(i>j);
+System.out.println(i==j);
+System.out.println(i<=j);
+System.out.println(i>=j);
+System.out.println(i!=j);
+
+
+System.out.println(j<i);
+System.out.println(j>i);
+System.out.println(j==i);
+System.out.println(j<=i);
+System.out.println(j>=i);
+System.out.println(j!=i);
+}
+}
