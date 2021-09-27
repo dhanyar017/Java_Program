b8a412c683fc97ee841bc6aa8e8160d8132ec8b0
dhanyar017 <dhanyaracharya017@gmail.com>
2021-09-26 22:01:26 -0400

    writes bitwise xor opertaion program

---
+++ BitwiseXOROperation.java
@@ -0,0 +1,17 @@
+class BitwiseXOROperation
+{
+public static void main(String[] args)
+{
+int x=5;
+int y=7;
+int z=3;
+int result=x^y;
+System.out.println("x^y : "+result);
+int result1=y^z;
+System.out.println("y^z : "+result1);
+int result2=x^z;
+System.out.println("x^z : "+result2);
+int result3=z^y;
+System.out.println("z^y : "+result3);
+}
+}
