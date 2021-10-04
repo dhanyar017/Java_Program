4309905151035e0df0ad6a042a676b305bec8983
dhanyar017 <dhanyaracharya017@gmail.com>
2021-10-03 21:58:33 -0400

    writes digits program

---
+++ Digit2.java
@@ -0,0 +1,15 @@
+class Digit2
+{
+public static void main(String[] args)
+{
+int num=10090;
+if(num<1000000 && num>=100000)
+{
+System.out.println(num+" is a six digit number!!!!!!!");
+}
+else
+{
+System.out.println(num+" is not a six digit number!!!!!!!");
+}
+}
+}
