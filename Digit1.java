1717da34651abcdcbed5baa8d718c74646fe1b50
dhanyar017 <dhanyaracharya017@gmail.com>
2021-10-03 21:57:26 -0400

    writes digits program

---
+++ Digit1.java
@@ -0,0 +1,15 @@
+class Digit1
+{
+public static void main(String[] args)
+{
+int num=10090;
+if(num<10000 && num>=1000)
+{
+System.out.println(num+" is a four digit number!!!!!!!");
+}
+else
+{
+System.out.println(num+" is not a four digit number!!!!!!!");
+}
+}
+}
