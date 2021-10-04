36a1ba3b6f01ccde4905bcc4c9758c257bd291c8
dhanyar017 <dhanyaracharya017@gmail.com>
2021-10-03 22:04:14 -0400

    writes printing days program

---
+++ PrintDays.java
@@ -0,0 +1,26 @@
+class PrintDays
+{
+public static void main(String[] args)
+{
+int number=5;
+
+switch(number)
+{
+case 1 : System.out.println("SUNDAY");
+         break;
+case 2 : System.out.println("MONDAY");
+         break;
+case 3 : System.out.println("TUESDAY");
+         break;
+case 4 : System.out.println("WEDNESDAY");
+         break;
+case 5 : System.out.println("THURSDAY");
+         break;
+case 6 : System.out.println("FRIDAY");
+         break;
+case 7 : System.out.println("SATURDAY");
+         break;
+default : System.out.println("...Enter valid number.....");
+}
+}
+}
