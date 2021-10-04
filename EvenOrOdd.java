5e554f148eec00021987008edcbc357fdc62d5f4
dhanyar017 <dhanyaracharya017@gmail.com>
2021-10-03 21:59:44 -0400

    writes even or odd program

---
+++ EvenOrOdd.java
@@ -0,0 +1,16 @@
+class EvenOrOdd
+{
+public static void main(String[] args)
+{
+int n=10;
+
+if(n%2==0)
+{
+System.out.println(".........EVEN  NUMBER..........");
+}
+else
+{
+System.out.println(".........ODD  NUMBER..........");
+}
+}
+}
