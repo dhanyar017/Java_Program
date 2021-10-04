e369579f2bc5ca8c2490c34b457af5661009a1b6
dhanyar017 <dhanyaracharya017@gmail.com>
2021-10-03 21:51:17 -0400

    writes boolean operation program

---
+++ BooleanDemo.java
@@ -0,0 +1,16 @@
+class BooleanDemo
+{
+public static void main(String[] args)
+{
+boolean isDevelopers=false;
+
+if(isDevelopers)
+{
+System.out.println(".........HELLO DEVELOPERS..........");
+}
+else
+{
+System.out.println(".........HELLO JOKERS..........");
+}
+}
+}
