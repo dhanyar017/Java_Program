90b3a7044c1b43e4999f1fbff87e22cceca939cd
dhanyar017 <dhanyaracharya017@gmail.com>
2021-10-03 22:00:56 -0400

    writes long operator program

---
+++ LongDemo.java
@@ -0,0 +1,16 @@
+class LongDemo
+{
+public static void main(String[] args)
+{
+int n=-765;
+
+if(n>10)
+{
+System.out.println(".........VALID NUMBER..........");
+}
+else
+{
+System.out.println(".........INVALID  NUMBER..........");
+}
+}
+}
