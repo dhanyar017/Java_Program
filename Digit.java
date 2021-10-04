05fdf8c72846cf58167584dee038465f6c879b47
dhanyar017 <dhanyaracharya017@gmail.com>
2021-10-03 21:55:50 -0400

    writes digits program

---
+++ Digit.java
@@ -0,0 +1,12 @@
+class Digit
+{
+public static void main(String[] args)
+{
+int num=25;
+if(num<100 && num>=10)
+{
+System.out.println(num+" is a two digit number!!!!!!!");
+}
+else
+{
+System.out.println(num+" is not a two digit number!!!!!!!");
